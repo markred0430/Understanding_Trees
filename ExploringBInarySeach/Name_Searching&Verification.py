54757e9b7653c6e58de30539901e6d652fb9300c
markred0430 <redilamarkanthony30@gmail.com>
2023-01-15 19:28:24 +0800

    Search function DONE.

--- ExploringBInarySeach/Name_Searching&Verification.py
+++ ExploringBInarySeach/Name_Searching&Verification.py
@@ -31,4 +31,20 @@ def build_tree(elements):
 # execution of tree
 
 # make function for search
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
 # Use in order traversal for sorting
