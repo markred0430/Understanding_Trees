57e7d9a05a78c141c08988ab196eb2893e9b1c85
markred0430 <redilamarkanthony30@gmail.com>
2023-01-15 19:17:30 +0800

    Steps 1-3 DONE.

--- ExploringBInarySeach/Name_Searching&Verification.py
+++ ExploringBInarySeach/Name_Searching&Verification.py
@@ -1,6 +1,34 @@
 # Making a class for BinarySearch
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 # Addchild method for the class
+    def add_child(self, data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
 # build tree for the class
+def build_tree(elements):
+    print("Building tree with these elements:",elements)
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
 # execution of tree
+
 # make function for search
 # Use in order traversal for sorting
